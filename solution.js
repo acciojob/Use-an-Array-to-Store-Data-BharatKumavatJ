1dfab6e34eb3f2f842d7f8886f523358ddc9de52
BharatKumavatJ <98304786+BharatKumavatJ@users.noreply.github.com>
2022-07-08 19:11:33 +0530

    array defined

--- solution.js
+++ solution.js
@@ -2,6 +2,8 @@
 let yourArray;
 // assign your array the correct value in the next line
 // your code here!
+var doIloveCoding = true;
+var myarray = ["bharat", 50, {"name":"bharat", "age":21}, doIloveCoding ];
 // code end
 
 module.exports = yourArray
